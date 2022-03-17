66c5558ec70b557a673492a79850a4ed2d1daa43
FotisKogias <fotiskogias@hotmail.com>
2022-03-17 21:40:02 +0200

    user-profile-socket

---
+++ api/controllers/rest-api/user-profile.js
@@ -0,0 +1,59 @@
+module.exports = {
+
+
+  friendlyName: 'User profile',
+
+
+  description: '',
+
+
+  inputs: {
+    userId: { type: 'number', required: true },
+    offerOrReceive: { type: 'string', required: true }
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+    let user = await TestUser.findOne({
+      where: { id: inputs.userId },
+      select: ['firstName', 'email', "dateOfBirth"]
+    })
+    
+    let userReceivingArrangements = await Arrangement.find({
+      where: {
+        receiving_user_id: user.id,
+        status: { in: ['finished', 'inProgress'] }
+      },
+      select: ['listing_id']
+    })
+
+    let userOfferingArrangements = await Arrangement.find({
+      where: {
+        offering_user_id: user.id,
+        status: { in: ['finished', 'inProgress'] }
+      },
+      select: ['listing_id']
+    })
+
+    if (inputs.offerOrReceive == "offering"){
+      var numbers = userOfferingArrangements.map(element => element.listing_id)
+    } else if (inputs.offerOrReceive == "receiving"){
+      var numbers = userReceivingArrangements.map(element => element.listing_id)
+    }
+    
+
+    let userListing = await Listing.find({
+      where: { id: { in: numbers } }
+    }).populate('arrangements')
+    return userListing;
+
+
+  }
+
+
+};
