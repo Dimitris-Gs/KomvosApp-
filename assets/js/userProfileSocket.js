084f157d0bbaf72dec87eb32f48a273180064f5b
Evangelap <evapapageorgiou@hotmail.com>
2022-03-18 11:19:28 +0200

    listings div dynamically rendered

--- assets/js/userProfileSocket.js
+++ assets/js/userProfileSocket.js
@@ -8,7 +8,7 @@ document.getElementById("received").addEventListener('click', () => {
     // console.log("hello from received");
     // getReceivingListings()
     getListings(document.getElementById("received").value)
-    
+
     // console.log(document.getElementById("received").value);
 })
 
@@ -26,11 +26,40 @@ function getListings(offeredOrReceived) {
             {
                 method: 'post',
                 url: '/userprofile',
-                data: { userId: 1, offerOrReceive: offerOrReceive}
+                data: { userId: 1, offerOrReceive: offerOrReceive }
             },
             function (result, response) {
                 console.log("hello from result");
                 console.log(result);
+                let root = document.getElementById("root");
+                
+                let listings = `<h1> Listings </h1>`
+                for (let i = 0; i < result.length; i++) {
+                    
+                    for (let j = 0; j < result[i].arrangements.length; j++) {
+                        listings += `
+                        <div class="card" style="width: 50%; margin: 0 auto;">
+                            <p>
+                                <a data-bs-toggle="collapse" href="#collapse${result[i].arrangements[j].id}" role="button" aria-expanded="false" aria-controls="collapseExample" style="text-decoration: none; color: black;">
+                                    Προσέφερες την ${result[i].name} στον ${result[i].arrangements[j].receiving_user_id}
+                                </a>                
+                            </p>
+
+                            <div class="collapse" id="collapse${result[i].arrangements[j].id}">
+                                <div class="card-body">
+                                    Ημερομηνία έναρξης: ${new Date().toLocaleDateString(result[i].startingDate)}
+                                                Ημερομηνία λήξης: ${new Date().toLocaleDateString(result[i].endingDate)}
+                                                Περιγραφή: ${result[i].description}
+                                                Κατηγορία: ${result[i].category_id}
+                                </div>
+                            </div>
+                        </div>`
+
+                        root.innerHTML = listings
+                    }
+                }
+
+
             }
         );
     });
