aeef0cc811479bb5d8026bc9f57125dbc322ea44
FotisKogias <fotiskogias@hotmail.com>
2022-04-16 18:34:46 +0300

    1.0

--- eva/assets/js/userprofile.js
+++
@@ -1,9 +0,0 @@
-const editProfile = document.getElementById("editform");
-const btn = document.getElementById("toggle");
-btn.onclick = function () {
-  if (editProfile.style.display !== "none") {
-    editProfile.style.display = "none";
-  } else {
-    editProfile.style.display = "block";
-  }
-};
